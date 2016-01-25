5593c0d2aae865bc4a4e54dde26abb52b964f3b5
Pushkar <pushkar@cytobank.org>
2016-01-25 17:34:23 +0530

    added  more examples,closures,IIFE,callbacks,call,binf ,appy etc.

---
+++ closures.js
@@ -0,0 +1,59 @@
+/* Understand Closures */
+function greet(whattosay) {
+
+   return function(name) {
+       console.log(whattosay + ' ' + name);
+   }
+
+}
+
+var sayHi = greet('Hi');
+sayHi('Pushkar');
+
+
+function buildFunctions() {
+ 
+    var arr = [];
+    
+    for (var i = 0; i < 3; i++) {
+        
+        arr.push(
+            function() {
+                console.log(i);   
+            }
+        )
+        
+    }
+    
+    return arr;
+}
+
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
+
+function buildFunctions2() {
+ 
+    var arr = [];
+    
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j);   
+                }
+            }(i))
+        )
+        
+    }
+    
+    return arr;
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
---
+++ closuresANDcallbacks.js
@@ -0,0 +1,47 @@
+function sayHiLater() {
+ 
+    var greeting = 'Hi!';
+    
+    setTimeout(function() {
+        
+        console.log(greeting);
+        
+    }, 3000);
+    
+}
+
+sayHiLater();
+
+// jQuery uses function expressions and first-class functions!
+//$("button").click(function() {
+//    
+//});
+
+function tellMeWhenDone(callback) {
+ 
+    var a = 1000; // some work
+    var b = 2000; // some work
+    
+    callback(); // the 'callback', it runs the function I give it!
+    
+}
+
+tellMeWhenDone(function() {
+   
+    setTimeout(function() {
+        
+        console.log("I am done");
+        
+    }, 2000);
+    
+});
+
+tellMeWhenDone(function() {
+   
+    setTimeout(function() {
+        
+        console.log("All Done");
+        
+    }, 4000);
+    
+});
