5c8d885cfccd53d292436142c0ee6a8e00c03bf8
Pushkar <pushkar@cytobank.org>
2016-01-28 18:42:28 +0530

    Added examples of prototype chaina nd reflection.

---
+++ ReflectionAndExtend.js
@@ -0,0 +1,48 @@
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;  
+    }
+}
+
+var john = {
+    firstname: 'John',
+    lastname: 'Doe'
+}
+
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+
+for (var prop in john) {
+    if (john.hasOwnProperty(prop)) {
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+
+var jane = {
+    address: '111 Main St.',
+    getFormalFullName: function() {
+        return this.lastname + ', ' + this.firstname;   
+    }
+}
+
+var jim = {
+    getFirstName: function() {
+        return firstname;   
+    }
+}
+// Include underscore.js in index.html
+//<script src="PATH/underscore.js"></script>
+
+_.extend(john, jane, jim);
+
+console.log(john);
+
+
+
+
+
+
+
+
