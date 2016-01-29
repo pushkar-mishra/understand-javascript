4bd057b6ccb81f7c59bdb1e13a42771efcc0b929
Pushkar <pushkar@cytobank.org>
2016-01-29 20:20:14 +0530

    Prototype, fun Ctor, and prototypal inhiritance.

--- prototypeInheritance.js
+++ prototypeInheritance.js
@@ -32,3 +32,30 @@ person.getFormalFullName = function() {
 
 console.log(john.getFormalFullName());
 console.log(jane.getFormalFullName());
+
+/************************************************************************/
+// polyfill
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation'
+      + ' only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+}
+
+var person1 = {
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstname;   
+    }
+}
+
+var john1 = Object.create(person1);
+john1.firstname = 'John';
+john1.lastname = 'Doe';
+console.log(john1);
